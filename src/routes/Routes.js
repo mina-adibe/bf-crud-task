fafd9d55ea569b758d7f5c9c4f0a030479f82ded
mina bessada <mina.adibe@gmail.com>
2022-08-28 08:24:47 +0200

    fix issue in routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../component/header/Header";
 
 import Home from "../pages/Home";
 import Page404 from "../pages/Page404";
@@ -8,6 +9,7 @@ import PostDetails from "../pages/PostDetails";
 const RoutesComponent = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<PostDetails />} />
